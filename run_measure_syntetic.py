8e5a11a75e0c445bef63f3afc0150a1c8976fec9
Hans Märki <buhtig.hans.maerki@ergoinfo.ch>
2020-02-08 22:21:22 +0100

    refactoring

--- run_measure_syntetic.py
+++ run_measure_syntetic.py
@@ -9,6 +9,13 @@ class TestSignal:
   def __init__(self, sine_amp_V_rms, noise_density_V_sqrtHz):
     self.sine_amp_V_rms = sine_amp_V_rms
     self.noise_density_V_sqrtHz = noise_density_V_sqrtHz
+    self.list_frequencies = []
+    sine_freq_Hz = 0.000001 # on a E12 point, not equal to fs so we can see errors, just in case
+    maxNyquistHz = 1/(2.0*dt_s)
+    while sine_freq_Hz < 0.8 * maxNyquistHz: # only sines below nyquist limit
+      self.list_frequencies.append(sine_freq_Hz)
+      print(f'sine_freq_Hz: {sine_freq_Hz}')
+      sine_freq_Hz = 10.0 * sine_freq_Hz # a sine every decade
 
   def calculate(self, dt_s, sample_start, push_size_samples):
     # test signal to test the algorythm
@@ -16,16 +23,11 @@ class TestSignal:
     # you should be able to see the given values in the measurement
     time_0 = sample_start * dt_s
     time = np.arange(push_size_samples) * dt_s + time_0
-    array = np.zeros(push_size_samples)
-    maxNyquistHz = 1/(2.0*dt_s)
-    sine_freq_Hz = 0.000001 # on a E12 point, not equal to fs so we can see errors, just in case
-    while sine_freq_Hz < 0.8 * maxNyquistHz: # only sines below nyquist limit
-      array += self.sine_amp_V_rms*np.sqrt(2)*np.sin(2*np.pi*sine_freq_Hz*time)
-      # print(sine_freq_Hz)
-      sine_freq_Hz = 10.0 * sine_freq_Hz # a sine every decade
-    array += np.random.normal(scale=self.noise_density_V_sqrtHz*np.sqrt(1/(2.0*dt_s)), size=push_size_samples)
-    assert len(array) == push_size_samples
-    return array
+    signal = np.random.normal(scale=self.noise_density_V_sqrtHz*np.sqrt(1/(2.0*dt_s)), size=push_size_samples)
+    for sine_freq_Hz in self.list_frequencies:
+      signal += self.sine_amp_V_rms*np.sqrt(2)*np.sin(2*np.pi*sine_freq_Hz*time)
+    assert len(signal) == push_size_samples
+    return signal
 
 if __name__ == '__main__':
   class SampleProcessConfig:
