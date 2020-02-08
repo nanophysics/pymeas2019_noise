5efb18a82811d5abe2405a1f2833cbc6779791b9
peter.maerki <git.peter@ergoinfo.ch>
2020-02-08 18:01:14 +0100

    .

--- todo/peter/tests/noise_generator.py
+++ todo/peter/tests/noise_generator.py
@@ -12,14 +12,15 @@ def process(sample_start, samples, dt_s, sine_amp_V_rms = 0.1, noise_density_V_s
     time = np.arange(samples) * dt_s + time_0
     x = np.zeros(samples)
     maxNyquistHz = 1/(2.0*dt_s)
-    sine_freq_Hz = 0.000015 # on a E12 point, not equal to fs so we can see errors, just in case
+    sine_freq_Hz = 0.00001 # on a E12 point, not equal to fs so we can see errors, just in case
     while sine_freq_Hz < 0.8 * maxNyquistHz: # only sines below nyquist limit
       x += sine_amp_V_rms*np.sqrt(2)*np.sin(2*np.pi*sine_freq_Hz*time)
+      print(sine_freq_Hz)
       sine_freq_Hz = 10.0 * sine_freq_Hz # a sine every decade
     x += np.random.normal(scale=noise_density_V_sqrtHz*np.sqrt(1/(2.0*dt_s)), size=samples)
     return(x)
 
-x = process(sample_start=0, samples=1000, dt_s=dt_s, sine_amp_V_rms = 0.1, noise_density_V_sqrtHz = 0.1)
+x = process(sample_start=0, samples=10000, dt_s=dt_s, sine_amp_V_rms = 0.1, noise_density_V_sqrtHz = 0.001)
 
 if True:
     f, Pxx_den = signal.periodogram(x, 1/dt_s, window='hamming', detrend='linear')
