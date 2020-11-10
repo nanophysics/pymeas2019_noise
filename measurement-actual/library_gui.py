557d78c23e2ba9305f48028d013ccde2893b4eac
Hans Märki <hans.maerki@zhinst.com>
2020-11-10 10:50:16 +0100

    library_gui ready to add animation

--- measurement-actual/library_gui.py
+++ measurement-actual/library_gui.py
@@ -1,33 +1,17 @@
-"""
-==================
-Embedding in wx #3
-==================
+'''
+This code is base on a sample from
+  Copyright (C) 2003-2004 Andrew Straw, Jeremy O'Donoghue and others
 
-Copyright (C) 2003-2004 Andrew Straw, Jeremy O'Donoghue and others
-
-License: This work is licensed under the PSF. A copy should be included
-with this source code, and is also available at
-https://docs.python.org/3/license.html
-
-This is yet another example of using matplotlib with wx.  Hopefully
-this is pretty full-featured:
-
-- both matplotlib toolbar and WX buttons manipulate plot
-- full wxApp framework, including widget interaction
-- XRC (XML wxWidgets resource) file to create GUI (made with XRCed)
-
-This was derived from embedding_in_wx and dynamic_image_wxagg.
-
-Thanks to matplotlib and wx teams for creating such great software!
-"""
+  License: This work is licensed under the PSF. A copy should be included
+  with this source code, and is also available at
+  https://docs.python.org/3/license.html
+'''
 
 import pathlib
 
 import matplotlib.cm as cm
 import matplotlib.cbook as cbook
-from matplotlib.backends.backend_wxagg import (
-    FigureCanvasWxAgg as FigureCanvas,
-    NavigationToolbar2WxAgg as NavigationToolbar)
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg, NavigationToolbar2WxAgg
 from matplotlib.figure import Figure
 import numpy as np
 
@@ -36,17 +20,22 @@ import wx.xrc as xrc
 
 ERR_TOL = 1e-5  # floating point slop for peak-detection
 
+class PlotContext:
+    def __init__(self, figure, do_animate, func_animate=None):
+        self.figure = figure
+        self.do_animate = do_animate
+        self.func_animate = func_animate
 
 class PlotPanel(wx.Panel):
-    def __init__(self, parent):
+    def __init__(self, parent, plot_context):
         wx.Panel.__init__(self, parent, -1)
+        self.fig = plot_context.figure
 
-        self.fig = Figure((5, 4), 75)
-        self.canvas = FigureCanvas(self, -1, self.fig)
-        self.toolbar = NavigationToolbar(self.canvas)  # matplotlib toolbar
+        self.canvas = FigureCanvasWxAgg(self, -1, self.fig)
+        self.toolbar = NavigationToolbar2WxAgg(self.canvas)  # matplotlib toolbar
         self.toolbar.Realize()
 
-        # Now put all into a sizer
+        # Now put all into a sizer which will resize the figure when the window size changes
         sizer = wx.BoxSizer(wx.VERTICAL)
         # This way of adding to sizer allows resizing
         sizer.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
@@ -56,19 +45,20 @@ class PlotPanel(wx.Panel):
         self.Fit()
 
     def init_plot_data(self):
-        ax = self.fig.add_subplot(111)
+        if False:
+            ax = self.fig.add_subplot(111)
 
-        x = np.arange(120.0) * 2 * np.pi / 60.0
-        y = np.arange(100.0) * 2 * np.pi / 50.0
-        self.x, self.y = np.meshgrid(x, y)
-        z = np.sin(self.x) + np.cos(self.y)
-        self.im = ax.imshow(z, cmap=cm.RdBu, origin='lower')
+            x = np.arange(120.0) * 2 * np.pi / 60.0
+            y = np.arange(100.0) * 2 * np.pi / 50.0
+            self.x, self.y = np.meshgrid(x, y)
+            z = np.sin(self.x) + np.cos(self.y)
+            self.im = ax.imshow(z, cmap=cm.RdBu, origin='lower')
 
-        zmax = np.max(z) - ERR_TOL
-        ymax_i, xmax_i = np.nonzero(z >= zmax)
-        if self.im.origin == 'upper':
-            ymax_i = z.shape[0] - ymax_i
-        self.lines = ax.plot(xmax_i, ymax_i, 'ko')
+            zmax = np.max(z) - ERR_TOL
+            ymax_i, xmax_i = np.nonzero(z >= zmax)
+            if self.im.origin == 'upper':
+                ymax_i = z.shape[0] - ymax_i
+            self.lines = ax.plot(xmax_i, ymax_i, 'ko')
 
         self.toolbar.update()  # Not sure why this is needed - ADS
 
@@ -91,8 +81,11 @@ class PlotPanel(wx.Panel):
 
         self.canvas.draw()
 
-
 class MyApp(wx.App):
+    def __init__(self, plot_context):
+        self._plot_context = plot_context
+        wx.App.__init__(self)
+
     def OnInit(self):
         xrcfile = pathlib.Path(__file__).absolute().with_suffix('.xrc')
         print('loading', xrcfile)
@@ -112,7 +105,7 @@ class MyApp(wx.App):
         sizer = wx.BoxSizer(wx.VERTICAL)
 
         # matplotlib panel itself
-        self.plotpanel = PlotPanel(plot_container)
+        self.plotpanel = PlotPanel(plot_container, self._plot_context)
         self.plotpanel.init_plot_data()
 
         # wx boilerplate
@@ -173,5 +166,6 @@ class MyApp(wx.App):
 
 
 if __name__ == '__main__':
-    app = MyApp()
+    plot_context = PlotContext(figure=Figure((5, 4), 75), do_animate=True)
+    app = MyApp(plot_context)
     app.MainLoop()
