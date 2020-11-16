a4ec852ff1c253d9189de7e6351c2d823e7db21d
Hans Märki <hans.maerki@zhinst.com>
2020-11-16 18:25:58 +0100

    Moved diagrams

--- documentation/developer_diagrams/run_diagrams.sh
+++ documentation/developer_diagrams/run_diagrams.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+cd ../..
+
 pyreverse --output=dot --project=FIR program_classify.py program_fir.py program_eseries.py
 pyreverse --output=dot --project=pymeas2019_noise --ignore=database.py *.py measurement-actual/*.py
 
-mv classes_* documentation_design
-mv packages_* documentation_design
+mv classes_* documentation/developer_diagrams
+mv packages_* documentation/developer_diagrams
