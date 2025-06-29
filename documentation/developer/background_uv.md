d6e2d60ce34fbb94545c11aed647df1e550e4f7b
Hans Märki <hans.maerki@zhinst.com>
2025-06-29 09:11:10 +0200

    After uv install: install python 3.13.5

---
+++ documentation/developer/background_uv.md
@@ -0,0 +1,9 @@
+# UV
+
+## Clean uv under windows
+
+C:\Users\maerki\.local\bin
+C:\Users\maerki\AppData\Local\uv
+C:\Users\maerki\AppData\Roaming\uv
+
+## uv python list
--- measurement_actual/run_9_install_uv.ps1
+++ measurement_actual/run_9_install_uv.ps1
@@ -2,6 +2,8 @@
 
 powershell -ExecutionPolicy Bypass -Command "irm https://astral.sh/uv/install.ps1 | iex"
 
+uv python install 3.13.5
+
 # Prompt and wait for any key press
 Write-Host "`nType any key to terminate..."
 [void][System.Console]::ReadKey($true)
